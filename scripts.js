db25a6ce3a7a9bf2ca354277f986f34904bfeee8
Thiago Moreira <thiagomf2@gmail.com>
2026-05-05 20:54:43 -0300

    finalização do site, basico porem funcional, posso adaptar para um uso futuro em algum produto

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log("Script carregado com sucesso!");
+const formulario = document.querySelector(".formulario");
+const mascara = document.querySelector(".mascara-form");
+
+function cliqueiNoBotao() {
+    formulario.style.left = "50%";
+    formulario.style.transform = "translateX(-50%)";
+    mascara.style.visibility = "visible";
+}
+
+function esconderform() {
+    formulario.style.left = "-340px";
+    formulario.style.transform = "translateX(0)";
+    mascara.style.visibility = "hidden";   
+}
+
